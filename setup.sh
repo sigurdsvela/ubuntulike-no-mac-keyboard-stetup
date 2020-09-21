3ac235aed3c940928938e99b11d708c87a52ca9b
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2020-09-21 16:10:23 +0200

    Path shit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
-cd $(dirname $0)
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
 
 ## Add the custom layout to the "NO" xkb layout file
 echo "- Create custom keybaord map"
-sudo ln -fs ./no-mac-xkb/no_mac.xkb-symbol /usr/share/X11/xkb/symbols/no_mac
+if [ ! -d /usr/share/X11/xkb/symbols/ ]; then sudo mkdir -p /usr/share/X11/xkb/symbols/; fi
+sudo ln -fs $(pwd)/no-mac-xkb/no_mac.xkb-symbol /usr/share/X11/xkb/symbols/no_mac
 
 # Make sure we have xkeysnail and kinto dependencies
 echo "- apt update"
@@ -15,7 +25,7 @@ echo "- Setup kinto; This part will be interactive!"
 sleep 2
 
 # Setup kinto
-./kinto/setup
+./kinto/setup.sh
 
 
 cd -
